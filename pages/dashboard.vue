91befea986535d8108c9afef9ebe312513093ef3
Mantai59 <mmantaibusiness@gmail.com>
2023-07-16 16:18:12 +0200

    feat: protecting auth routes client side

---
+++ pages/dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <main class="flex flex-col grow">
+        Dashboard 
+    </main>
+</template>
+
+<script setup lang="ts">
+    const user = useSupabaseUser()
+
+    onMounted(() => {
+        watchEffect(() => {
+            if(!user.value) {
+                navigateTo("/")
+            }
+        })
+    })
+
+    definePageMeta({
+        middleware: ['auth']
+    })
+</script>
